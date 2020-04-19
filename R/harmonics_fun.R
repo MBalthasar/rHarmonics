03ee92b07429d3126645b706d98749ee5b2364d8
MBalthasar <maphi@maxi-dsl.de>
2020-04-19 12:12:55 +0200

    Allowed for user values that consist of NA values only. In this case, the output will be the same as the input. Necessary for applying the function on rasters with pixels that consists only NA.

--- R/harmonics_fun.R
+++ R/harmonics_fun.R
@@ -81,91 +81,101 @@ harmonics_fun <- function(user_vals, user_dates, harmonic_deg, ref_date){
     stop("Reference date must be provided as a 'Date' object.")
   }
 
-  ### Calculate difference to ref_date in radians ###
+  # If user vals are only NA, output is same as input
 
-  # Start for loop
-  for (i in 1:length(user_dates)){
-    current_date <- user_dates[i]
-    # Calculate the difference in days
-    current_diff_days <- as.numeric(difftime(current_date, ref_date), units="days")
-    # Convert to years
-    current_diff_years <- current_diff_days/365.25
-    # Convert to radians
-    current_diff_radians <- current_diff_years * 2 * pi
-    if (i == 1){
-      my_radians <- current_diff_radians
-    } else {
-      my_radians <- c(my_radians, current_diff_radians)
+  if (all(is.na(user_vals))) {
+    print("User values consist of NA values only. Output is same as input")
+    return(user_vals)
+
+    # Otherwise apply harmonic analysis
+  } else {
+
+    ### Calculate difference to ref_date in radians ###
+
+    # Start for loop
+    for (i in 1:length(user_dates)){
+      current_date <- user_dates[i]
+      # Calculate the difference in days
+      current_diff_days <- as.numeric(difftime(current_date, ref_date), units="days")
+      # Convert to years
+      current_diff_years <- current_diff_days/365.25
+      # Convert to radians
+      current_diff_radians <- current_diff_years * 2 * pi
+      if (i == 1){
+        my_radians <- current_diff_radians
+      } else {
+        my_radians <- c(my_radians, current_diff_radians)
+      }
+      rm(i, current_date, current_diff_days, current_diff_years, current_diff_radians)
     }
-    rm(i, current_date, current_diff_days, current_diff_years, current_diff_radians)
+
+    ### Caculate sines and cosines ###
+
+    # Define constant
+    my_cons <- rep(1, times = length(my_radians))
+    # Create sines and cosines data frames with one column for each harmonic
+    my_sin <- data.frame(matrix(nrow = length(my_radians), ncol = harmonic_deg))
+    my_cos <- data.frame(matrix(nrow = length(my_radians), ncol = harmonic_deg))
+    # Create names for df
+    sin_names <- rep("sin_", times=harmonic_deg)
+    cos_names <- rep("cos_", times=harmonic_deg)
+    my_seq <- as.character(seq(1,harmonic_deg, by=1))
+    sin_names <- paste(sin_names, my_seq, sep="")
+    cos_names <- paste(cos_names, my_seq, sep="")
+    # Add names to df
+    names(my_sin) <- sin_names
+    names(my_cos) <- cos_names
+    # Calculate sines and cosines for each harmonic
+    for (j in 1:harmonic_deg){
+      # Calculate current sines and cosines by multiplying the radians with the current
+      # degree of harmonic and then apply the sine/cosine function
+      current_sines <- sin(my_radians * j)
+      current_cosines <- cos(my_radians * j)
+      # Fill data frames with values
+      my_sin[,j] <- current_sines
+      my_cos[,j] <- current_cosines
+      # remove redundant variables
+      rm(j, current_cosines, current_sines)
+    }
+    # Create df from the dependent and all independent variables
+    df_for_reg <- cbind(user_vals, my_cons, my_radians, my_sin, my_cos)
+
+    ### Apply Ordinary Least Squares Regression ###
+
+    # Ordinary Least Squares Regression
+    my_reg <- stats::lm(formula = user_vals ~ ., data = df_for_reg)
+    # Get coefficients (constant is intercept value)
+    # For coefficient and radians it's easy ...
+    cons_coef <- my_reg$coefficients[1]
+    t_coef <- my_reg$coefficients[3]
+    # ... for the sines and cosines selecting the right columns is a little more complicated
+    # Start with 4 because the first three values are the ndvi, my_cons and my_radians
+    # First define the start and stop column for the sines and cosines ...
+    sin_start <- 4
+    sin_end <- 4 + harmonic_deg - 1
+    cos_start <- 4 + harmonic_deg
+    cos_end <- 4 + harmonic_deg + harmonic_deg -1
+    # ... and then subset the data accordingly
+    sin_coef <- my_reg$coefficients[c(sin_start:sin_end)]
+    cos_coef <- my_reg$coefficients[c(cos_start:cos_end)]
+
+    ### Calculate fitted values ###
+
+    # multiply independent variables with the coefficients
+    df_for_reg[,2] <- df_for_reg[,2] * cons_coef
+    df_for_reg[,3] <- df_for_reg[,3] * t_coef
+    # for loop multiplying the factor for each harmonic degree
+    for (k in 1:harmonic_deg){
+      # for the sines define i + 3 because the first sine column is at the 4th position
+      df_for_reg[,k + 3] <- df_for_reg[,k + 3] * sin_coef[k]
+      # for the cosines define i + 3 + harmonic_deg to get to the first cosine column
+      df_for_reg[,k + 3 + harmonic_deg] <- df_for_reg[,k + 3 + harmonic_deg] * cos_coef[k]
+      # remove reduntant variables
+      rm(k)
+    }
+    # calculate sum (fitted value) of the multplied independent variables
+    fitted <- rowSums(df_for_reg[,c(2:ncol(df_for_reg))], na.rm = TRUE)
+    # return fitted values
+    return(fitted)
   }
-
-  ### Caculate sines and cosines ###
-
-  # Define constant
-  my_cons <- rep(1, times = length(my_radians))
-  # Create sines and cosines data frames with one column for each harmonic
-  my_sin <- data.frame(matrix(nrow = length(my_radians), ncol = harmonic_deg))
-  my_cos <- data.frame(matrix(nrow = length(my_radians), ncol = harmonic_deg))
-  # Create names for df
-  sin_names <- rep("sin_", times=harmonic_deg)
-  cos_names <- rep("cos_", times=harmonic_deg)
-  my_seq <- as.character(seq(1,harmonic_deg, by=1))
-  sin_names <- paste(sin_names, my_seq, sep="")
-  cos_names <- paste(cos_names, my_seq, sep="")
-  # Add names to df
-  names(my_sin) <- sin_names
-  names(my_cos) <- cos_names
-  # Calculate sines and cosines for each harmonic
-  for (j in 1:harmonic_deg){
-    # Calculate current sines and cosines by multiplying the radians with the current
-    # degree of harmonic and then apply the sine/cosine function
-    current_sines <- sin(my_radians * j)
-    current_cosines <- cos(my_radians * j)
-    # Fill data frames with values
-    my_sin[,j] <- current_sines
-    my_cos[,j] <- current_cosines
-    # remove redundant variables
-    rm(j, current_cosines, current_sines)
-  }
-  # Create df from the dependent and all independent variables
-  df_for_reg <- cbind(user_vals, my_cons, my_radians, my_sin, my_cos)
-
-  ### Apply Ordinary Least Squares Regression ###
-
-  # Ordinary Least Squares Regression
-  my_reg <- stats::lm(formula = user_vals ~ ., data = df_for_reg)
-  # Get coefficients (constant is intercept value)
-  # For coefficient and radians it's easy ...
-  cons_coef <- my_reg$coefficients[1]
-  t_coef <- my_reg$coefficients[3]
-  # ... for the sines and cosines selecting the right columns is a little more complicated
-  # Start with 4 because the first three values are the ndvi, my_cons and my_radians
-  # First define the start and stop column for the sines and cosines ...
-  sin_start <- 4
-  sin_end <- 4 + harmonic_deg - 1
-  cos_start <- 4 + harmonic_deg
-  cos_end <- 4 + harmonic_deg + harmonic_deg -1
-  # ... and then subset the data accordingly
-  sin_coef <- my_reg$coefficients[c(sin_start:sin_end)]
-  cos_coef <- my_reg$coefficients[c(cos_start:cos_end)]
-
-  ### Calculate fitted values ###
-
-  # multiply independent variables with the coefficients
-  df_for_reg[,2] <- df_for_reg[,2] * cons_coef
-  df_for_reg[,3] <- df_for_reg[,3] * t_coef
-  # for loop multiplying the factor for each harmonic degree
-  for (k in 1:harmonic_deg){
-    # for the sines define i + 3 because the first sine column is at the 4th position
-    df_for_reg[,k + 3] <- df_for_reg[,k + 3] * sin_coef[k]
-    # for the cosines define i + 3 + harmonic_deg to get to the first cosine column
-    df_for_reg[,k + 3 + harmonic_deg] <- df_for_reg[,k + 3 + harmonic_deg] * cos_coef[k]
-    # remove reduntant variables
-    rm(k)
-  }
-  # calculate sum (fitted value) of the multplied independent variables
-  fitted <- rowSums(df_for_reg[,c(2:ncol(df_for_reg))], na.rm = TRUE)
-  # return fitted values
-  return(fitted)
 }
